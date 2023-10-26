75d6913613f96a28963357458a70ecd00b3dfb4f
Stormer <tranhocpro2@gmail.com>
2023-10-26 22:31:32 +0700

    sửa tên project và làm được tẹo

---
+++ Store/API.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Store
+{
+    public partial class API : System.Web.UI.Page
+    {
+        const string cnStr = @"Server=STORMER;Database=Money;User id=hocdepzai;Password=hocdepzai";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string action = Request["action"];
+            switch(action)
+            {
+                case "check_login":
+                case "login":
+                case "signup":
+                    {
+                        user_action();
+                        break;
+                    }
+            }
+        }
+
+        void user_action() {
+            string action = Request["action"];
+            if (action == "login")
+            {
+
+            }
+        }
+    }
+}
